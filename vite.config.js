38352362333599671f0f91e141ce62e1fd8a9e3d
Joel <joel5vega@gmail.com>
2026-03-17 10:55:34 -0400

    fixed patrimonio

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/portafolio/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/patrimonio/' : '/',
 });
