b5c02ffe8055733409852c2eb2ee70689e7f2831
Joel <joel5vega@gmail.com>
2026-03-17 11:45:53 -0400

    deploy.yml

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/patrimonio/',   // ← fijo, sin condicional
+  base: process.env.NODE_ENV === 'production' ? '/patrimonio/' : '/',
+
 });
