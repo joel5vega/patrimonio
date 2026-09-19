50a4a0df155495fc19467744c522f35f342cc84b
Joel <joel5vega@gmail.com>
2026-09-19 14:35:19 -0400

    x

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -15,9 +15,9 @@ import PortfolioSecondaryDetails from '../features/portfolio/components/Portfoli
 import PortfolioSectorMap from '../features/portfolio/components/PortfolioSectorMap';
 import { refreshBinanceSnapshot } from '../lib/binanceSnapshotClient';
 import PortfolioPerformance from '../features/portfolio/components/PortfolioPerformance';
-import '../features/portfolio/styles/PortfolioPerformance.css';
-import '../features/portfolio/styles/portfolio.css';
 
+import '../features/portfolio/styles/portfolio.css';
+import '../features/portfolio/styles/PortfolioPerformance.css';
 
 export default function Portfolio() {
   const {
